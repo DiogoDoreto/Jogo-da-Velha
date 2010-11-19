67345f796c1fa0d46ba0b88c5237fb30e7fa4525
Diogo Doreto <diogo.doreto@gmail.com>
2010-11-19 16:31:05 -0200

    Primeiras definições

--- main.c
+++ main.c
@@ -8,9 +8,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+
+/**
+ * Exemplo do jogo:
+ *    x | o | x
+ *   ---|---|---
+ *    o | x |
+ *   ---|---|---
+ *    x | o |
  * 
+ * Definições:
+ *   - Jogada
+ *      * 0 - espaço vazio
+ *      * 1 - X
+ *      * 2 - O
+ *
+ *   - Jogadores
+ *      * 0 - CPU
+ *      * 1 - Humano 1
+ *      * 2 - Humano 2 (para multiplayer)
  */
+
+/**
+ * Guarda informações sobre a jogada
+ */
+struct jogada {
+    int m[3][3]; // Matriz da jogada
+    int ultJogador; // ultimo Jogador
+    struct jogada *proxima; // Próxima jogada
+};
+typedef struct jogada tipoJogada;
+
+/**
+ * Armazena o histórico de jogadas (Pilha)
+ */
+struct historico {
+    tipoJogada *primeira; // 1ª jogada
+    int qtd; // Qtd de jogadas
+};
+typedef struct historico tipoHistorico;
+
+
+
 int main(int argc, char** argv) {
 
     return (EXIT_SUCCESS);
