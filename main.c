5a509da822948272ece80e60bbba111474a7d57e
Diogo Doreto <diogo.doreto@gmail.com>
2010-11-19 15:38:56 -0200

    Inicio do projeto

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Diogo
+ *
+ * Created on 19 de Novembro de 2010, 15:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
