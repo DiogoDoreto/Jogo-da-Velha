92f7e118c7233f7d4a7f318bebf6190ff97460bc
Diogo Doreto <diogo.doreto@gmail.com>
2010-11-20 18:57:54 -0200

    Criacao dos arquivos de cabecalho auxiliares e funcao de inicializacao do jogo

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 /* 
  * File:   main.c
- * Author: Diogo
- *
- * Created on 19 de Novembro de 2010, 15:36
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "graficos.h"
+#include "funcoes_aux.h"
 
+/** Definições de constantes utilizadas no jogo */
+
+#define VALOR_VAZIO 0
+#define VALOR_X 1
+#define VALOR_O 2
+
+#define SINGLEPLAYER 0
+#define MULTIPLAYER 1
+
+#define CPU 0
+#define JOGADOR1 1
+#define JOGADOR2 2
 
 /**
  * Exemplo do jogo:
@@ -16,42 +28,79 @@
  *    o | x |
  *   ---|---|---
  *    x | o |
- * 
- * Definições:
- *   - Jogada
- *      * 0 - espaço vazio
- *      * 1 - X
- *      * 2 - O
- *
- *   - Jogadores
- *      * 0 - CPU
- *      * 1 - Humano 1
- *      * 2 - Humano 2 (para multiplayer)
  */
 
 /**
  * Guarda informações sobre a jogada
  */
-struct jogada {
-    int m[3][3]; // Matriz da jogada
+typedef struct jogada {
+    int jogo[3][3]; // Matriz da jogada
     int ultJogador; // ultimo Jogador
-    struct jogada *proxima; // Próxima jogada
-};
-typedef struct jogada tipoJogada;
+    struct jogada *anterior; // Jogada Anterior
+} tipoJogada;
 
 /**
- * Armazena o histórico de jogadas (Pilha)
+ * Armazena o histórico de jogadas (Pilha com poteiro sempre na última posição)
  */
-struct historico {
-    tipoJogada *primeira; // 1ª jogada
+typedef struct historico {
+    tipoJogada *ultima; // Última jogada
     int qtd; // Qtd de jogadas
-};
-typedef struct historico tipoHistorico;
+} tipoHistorico;
 
+/**
+ * Armazena o jogo e suas propriedades
+ */
+typedef struct jogo {
+    tipoHistorico historico;
+    int modoJogo; // SINGLEPLAYER ou MULTIPLAYER
+} tipoJogo;
 
+/**
+ * Inicia o jogo
+ * @param jogo Jogo a ser iniciado
+ * @param modo Modo do jogo [ SINGLEPLAYER | MULTIPLAYER ]
+ */
+void initJogo(tipoJogo *jogo, int modo);
 
 int main(int argc, char** argv) {
+    tipoJogo jogo;
+    int modo;
+
+    do {
+        printf("Escolha o modo de jogo:\n\n"
+                "  [0] Contra o PC;\n"
+                "  [1] Contra outra pessoa;\n\n-> ");
+        scanf(" %d", &modo);
+        initJogo(&jogo, modo);
+
+
+        printf("\nJogar de novo? [sim|nao]\n");
+    } while (confirma());
+
 
     return (EXIT_SUCCESS);
+}
+
+void initJogo(tipoJogo *jogo, int modo) {
+    jogo->modoJogo = modo;
+
+    // Inicia primeira jogada (em branco)
+    tipoJogada *jogada = malloc(sizeof(tipoJogada));
+    
+    // Inicializa matriz do jogo
+    int i = -1, j;
+    while (++i < 3)
+        while (++j < 3)
+            jogada->jogo[i][j] = 0;
+
+    // Sorteia quem irá iniciar o jogo
+    srand((unsigned int) time(NULL));
+    int jogador = (int) (rand() % 2);
+    jogada->ultJogador = jogador;
+
+    jogada->anterior = NULL;
+
+    jogo->historico.ultima = jogada;
+    jogo->historico.qtd = 1;
 }
 
